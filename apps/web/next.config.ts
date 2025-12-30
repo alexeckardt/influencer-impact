6ecd60ff06dde8f6c4a9434164701ed4edb2a889
Alex Eckardt <realalexeck@gmail.com>
2025-12-30 02:02:53 -0500

    More cloudflare stuff

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,8 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'standalone',
+  outputFileTracingIncludes: {
+    '/': ['./node_modules/**/*'],
+  },
   experimental: {
     optimizePackageImports: ['@supabase/supabase-js'],
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/@swc/core-linux-x64-gnu',
+        'node_modules/@swc/core-linux-x64-musl',
+        'node_modules/@esbuild/linux-x64',
+      ],
+    },
   },
 };
 
