b5fe8b198f404862e7cb94497c9fa99faba6dad8
Alex Eckardt <realalexeck@gmail.com>
2025-12-31 17:03:30 -0500

    Refactor next.config.mjs to remove output configuration and streamline settings

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
-  outputFileTracingIncludes: {
-    '/': ['./node_modules/**/*'],
-  },
-  outputFileTracingExcludes: {
-    '*': [
-      'node_modules/@swc/core-linux-x64-gnu',
-      'node_modules/@swc/core-linux-x64-musl',
-      'node_modules/@esbuild/linux-x64',
-    ],
-  },
   images: {
     remotePatterns: [
       {
